400cf5c5c41895e272f96b8ba9c015663888c15f
vero-martinez <veris.14@hotmail.com>
2026-07-01 15:15:23 -0500

    fix: remover @Builder de UsuarioResponseDTO para evitar conflicto con Jackson

--- src/main/java/com/parkcontrol/backend/client/dto/UsuarioResponseDTO.java
+++ src/main/java/com/parkcontrol/backend/client/dto/UsuarioResponseDTO.java
@@ -1,14 +1,8 @@
 package com.parkcontrol.backend.client.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class UsuarioResponseDTO {
     private Long id;
     private String nombres;
